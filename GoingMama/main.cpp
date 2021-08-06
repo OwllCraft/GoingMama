f885d646dc4c2e7637421b1615538bfe410a281a
OwllCraft <owllcraftme@gmail.com>
2021-08-06 15:36:53 +0800

    Published Debug version

--- GoingMama/main.cpp
+++ GoingMama/main.cpp
@@ -1,7 +1,14 @@
 #include "DEFINITIONS.hpp"
 #include "AppEngine.hpp"
+#include <Windows.h>
+
+void hideConsole() {
+	HWND myConsole = GetConsoleWindow();
+	ShowWindow(myConsole, 0);
+}
 
 int main() {
+	hideConsole();
     OwllCraft::AppEngine game(_SCREEN_WIDTH_, _SCREEN_HEIGHT_, "GoingMama");
     return 0;
 }
